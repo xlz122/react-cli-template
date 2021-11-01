ec9d1e9625a13d4f5f2ef7f14754d9e526f24cc7
xlz122 <122874722@qq.com>
2021-11-01 16:26:35 +0800

    fix: 去除原有路由判断代码

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,25 +1,10 @@
-import React, { useRef, Suspense } from 'react';
-import { Switch, Route, Link, useHistory } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 import routes from '@router/routes';
 import renderRoutes from '@router/renderRoutes';
 import NotFound from '@views/not-found/NotFound';
 
 function AppRouter(): React.ReactElement {
-  const history = useHistory();
-  const pathname = useRef();
-
-  // 直接刷新页面触发
-  if (!pathname.current) {
-    // console.log('路由未改变');
-    pathname.current = history.location.pathname;
-  }
-
-  // 路由改变时触发
-  if (pathname.current && history.location.pathname !== pathname.current) {
-    // console.log('路由改变');
-    pathname.current = history.location.pathname;
-  }
-
   return (
     <Switch>
       <Route path="/404" component={NotFound} />
