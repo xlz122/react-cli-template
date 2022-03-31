423d2db12d7b3a3fc344870ce06ec9986ac5a75e
xlz122 <122874722@qq.com>
2022-03-31 11:19:43 +0800

    chore: react、react-dom更新v18.0.0版本

--- src/index.tsx
+++ src/index.tsx
@@ -12,3 +12,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <AppRouter />
+//     </Provider>
+//   </React.StrictMode>,
+// );
